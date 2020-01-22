e5c5538ee1d2e65b6576eea508e3c12a033bd784
Angelo Lupo <angelolupo94@gmail.com>
2020-01-22 23:24:22 +0000

    api to receive json - prints to terminal for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+
+use rocket_contrib::json::{Json, JsonValue};
+
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    contents: String
+}
+
+#[put("/record", format = "json", data = "<message>")]
+fn record(message: Json<Message>) -> Option<JsonValue> {
+    print!("{:?}", message.0.contents);
+    Some(json!({ "status": "ok" }))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![record]).launch();
 }
